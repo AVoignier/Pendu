411096e10fdd5f40a6a2273e41a88a1124a1598e
Alexandre <Alexandre@SLEEPY>
2020-10-03 01:56:01 +0200

    Ajoutez des fichiers projet.

---
+++ Pendu/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Pendu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.imgPendu = new System.Windows.Forms.PictureBox();
+            this.lblInfos = new System.Windows.Forms.Label();
+            this.txbUtilisateur = new System.Windows.Forms.TextBox();
+            this.btnValider = new System.Windows.Forms.Button();
+            this.lblMotAffiche = new System.Windows.Forms.Label();
+            this.cmbLettres = new System.Windows.Forms.ComboBox();
+            this.btnQuitter = new System.Windows.Forms.Button();
+            this.lblGagneOuPerdu = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imgPendu)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imgPendu
+            // 
+            this.imgPendu.Image = global::Pendu.Properties.Resources.pendu0;
+            this.imgPendu.Location = new System.Drawing.Point(12, 12);
+            this.imgPendu.Name = "imgPendu";
+            this.imgPendu.Size = new System.Drawing.Size(365, 357);
+            this.imgPendu.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imgPendu.TabIndex = 0;
+            this.imgPendu.TabStop = false;
+            // 
+            // lblInfos
+            // 
+            this.lblInfos.AutoSize = true;
+            this.lblInfos.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInfos.Location = new System.Drawing.Point(385, 13);
+            this.lblInfos.Name = "lblInfos";
+            this.lblInfos.Size = new System.Drawing.Size(45, 16);
+            this.lblInfos.TabIndex = 1;
+            this.lblInfos.Text = "label1";
+            // 
+            // txbUtilisateur
+            // 
+            this.txbUtilisateur.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbUtilisateur.Location = new System.Drawing.Point(386, 32);
+            this.txbUtilisateur.Name = "txbUtilisateur";
+            this.txbUtilisateur.Size = new System.Drawing.Size(246, 26);
+            this.txbUtilisateur.TabIndex = 2;
+            this.txbUtilisateur.TextChanged += new System.EventHandler(this.txbUtilisateur_TextChanged);
+            // 
+            // btnValider
+            // 
+            this.btnValider.Location = new System.Drawing.Point(390, 278);
+            this.btnValider.Name = "btnValider";
+            this.btnValider.Size = new System.Drawing.Size(243, 40);
+            this.btnValider.TabIndex = 4;
+            this.btnValider.Text = "Valider";
+            this.btnValider.UseVisualStyleBackColor = true;
+            this.btnValider.Click += new System.EventHandler(this.btnValider_Click);
+            // 
+            // lblMotAffiche
+            // 
+            this.lblMotAffiche.AutoSize = true;
+            this.lblMotAffiche.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMotAffiche.Location = new System.Drawing.Point(386, 61);
+            this.lblMotAffiche.Name = "lblMotAffiche";
+            this.lblMotAffiche.Size = new System.Drawing.Size(86, 20);
+            this.lblMotAffiche.TabIndex = 5;
+            this.lblMotAffiche.Text = "MotAffiche";
+            // 
+            // cmbLettres
+            // 
+            this.cmbLettres.FormattingEnabled = true;
+            this.cmbLettres.Location = new System.Drawing.Point(388, 85);
+            this.cmbLettres.Name = "cmbLettres";
+            this.cmbLettres.Size = new System.Drawing.Size(121, 21);
+            this.cmbLettres.TabIndex = 6;
+            this.cmbLettres.SelectedIndexChanged += new System.EventHandler(this.cmbLettres_SelectedIndexChanged);
+            // 
+            // btnQuitter
+            // 
+            this.btnQuitter.Location = new System.Drawing.Point(390, 324);
+            this.btnQuitter.Name = "btnQuitter";
+            this.btnQuitter.Size = new System.Drawing.Size(243, 40);
+            this.btnQuitter.TabIndex = 7;
+            this.btnQuitter.Text = "Quitter";
+            this.btnQuitter.UseVisualStyleBackColor = true;
+            this.btnQuitter.Click += new System.EventHandler(this.btnQuitter_Click);
+            // 
+            // lblGagneOuPerdu
+            // 
+            this.lblGagneOuPerdu.AutoSize = true;
+            this.lblGagneOuPerdu.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGagneOuPerdu.Location = new System.Drawing.Point(390, 182);
+            this.lblGagneOuPerdu.Name = "lblGagneOuPerdu";
+            this.lblGagneOuPerdu.Size = new System.Drawing.Size(163, 24);
+            this.lblGagneOuPerdu.TabIndex = 8;
+            this.lblGagneOuPerdu.Text = "lblGagneOuPerdu";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 385);
+            this.Controls.Add(this.lblGagneOuPerdu);
+            this.Controls.Add(this.btnQuitter);
+            this.Controls.Add(this.cmbLettres);
+            this.Controls.Add(this.lblMotAffiche);
+            this.Controls.Add(this.btnValider);
+            this.Controls.Add(this.txbUtilisateur);
+            this.Controls.Add(this.lblInfos);
+            this.Controls.Add(this.imgPendu);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imgPendu)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox imgPendu;
+        private System.Windows.Forms.Label lblInfos;
+        private System.Windows.Forms.TextBox txbUtilisateur;
+        private System.Windows.Forms.Button btnValider;
+        private System.Windows.Forms.Label lblMotAffiche;
+        private System.Windows.Forms.ComboBox cmbLettres;
+        private System.Windows.Forms.Button btnQuitter;
+        private System.Windows.Forms.Label lblGagneOuPerdu;
+    }
+}
+
---
+++ Pendu/Form1.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pendu
+{
+    public partial class Form1 : Form
+    {
+
+        bool PartieEnCours;
+        string motJeu;
+        string motAffiche;
+        char[] LettresUtilisés = new char[26];
+        int viesJoueur;
+        Image[] pendues = new Image[11];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Initialisation des images de pendus
+            pendues[0] = Pendu.Properties.Resources.pendu10;
+            pendues[1] = Pendu.Properties.Resources.pendu9;
+            pendues[2] = Pendu.Properties.Resources.pendu8;
+            pendues[3] = Pendu.Properties.Resources.pendu7;
+            pendues[4] = Pendu.Properties.Resources.pendu6;
+            pendues[5] = Pendu.Properties.Resources.pendu5;
+            pendues[6] = Pendu.Properties.Resources.pendu4;
+            pendues[7] = Pendu.Properties.Resources.pendu3;
+            pendues[8] = Pendu.Properties.Resources.pendu2;
+            pendues[9] = Pendu.Properties.Resources.pendu1;
+            pendues[10] = Pendu.Properties.Resources.pendu0;
+
+            lblGagneOuPerdu.Text = "";
+
+            InitialisationPartie();
+
+        }
+
+        private void btnValider_Click(object sender, EventArgs e)
+        {
+            //Si la partie n'est pas en cours, le mot doit être saisie dans le champ tbxUtilisateur
+            if (!PartieEnCours)
+            {
+                string motSaisie = txbUtilisateur.Text;
+                //Vérification de la validité du mot
+
+                //Pas de mot
+                if (motSaisie.Length <= 0)
+                {
+                    lblInfos.Text = "Pas de mot saisie";
+                }
+                //Mot trop long
+                else if (motSaisie.Length > 15)
+                {
+                    lblInfos.Text = "Le mot doit comporter moins de 15 lettres";
+                }
+                //Longueur mot ok
+                else
+                {
+                    //Vérification de la validité du mot
+                    if (MotValide(motSaisie))
+                    {
+                        InitialisationDeviner(motSaisie);
+                    }
+                    else
+                    {
+                        lblInfos.Text = "Le mot ne doit contenir que des lettres";
+                    }
+
+
+                }
+
+            }
+            //Si la partie est en cours et que le joueur essaie de deviner le mot
+            else if(txbUtilisateur.Text != "")
+            {
+                //Vérification de la saisie de l'utilisateur
+                string motSaisie = txbUtilisateur.Text;
+                if (!MotValide(motSaisie))
+                {
+                    lblInfos.Text = "Le mot ne doit contenir que des lettres";
+                }
+                else if (motSaisie.Length > 15)
+                {
+                    lblInfos.Text = "Le mot ne peut pas contenir plus de 15 lettres";
+                }
+                else 
+                {
+                    DevinerMot(motSaisie);
+                }
+            }
+            //Sinon le joueur essaie de choisir une lettre
+            else
+            {
+                //Si aucune lettre n'est selectionné
+                if(cmbLettres.SelectedIndex == -1)
+                {
+                    lblInfos.Text = "Aucune lettre n'est selectionné";
+                }
+                else
+                {
+                    DevinerLettre();
+                }
+            }
+        }
+
+        void InitialisationPartie()
+        {
+            PartieEnCours = false;
+            lblMotAffiche.Text = "";
+            lblInfos.Text = "Choisissez un mot à faire deviner";
+            PartieEnCours = false;
+
+            cmbLettres.Items.Clear();
+
+        }
+
+        void InitialisationDeviner(string mot)
+        {
+            viesJoueur = 10;
+            imgPendu.Image = pendues[viesJoueur];
+
+            PartieEnCours = true;
+            txbUtilisateur.Text = "";
+
+            motJeu = mot;
+            motJeu =  motJeu.ToUpper();
+
+            //Initialisation de cmbLettres
+            for (int i = 0; i < 26; i++)
+            {
+                cmbLettres.Items.Add(char.ConvertFromUtf32((int)'A' + i));
+            }
+
+            //Création du mot Affiché
+            motAffiche = "_";
+
+            for (int i = 1; i < motJeu.Length; i++)
+            {
+                motAffiche += " _";
+            }
+            lblMotAffiche.Text = motAffiche;
+            lblInfos.Text = "Choisissez une lettre dans le menu puis validez";
+        }
+
+        void DevinerMot(string motSaisie)
+        {
+            motSaisie = motSaisie.ToUpper();
+            //Si le mot correspond
+            if (motSaisie == motJeu)
+            {
+                Gagne();
+
+            }
+            else
+            {
+                viesJoueur--;
+                imgPendu.Image = pendues[viesJoueur];
+                txbUtilisateur.Text = "";
+                if (viesJoueur == 0)
+                {
+                    Perdu();
+                }
+            }
+
+        }
+
+        void DevinerLettre()
+        {
+            char caractere = cmbLettres.SelectedItem.ToString()[0];
+
+            //Si la lettre est bien dans le mot
+            if ( motJeu.Contains( caractere.ToString() ))
+            {
+
+                //Remplacer les caractere correspondant dans motAffiche
+                for(int i=0; i<motJeu.Length; i++)
+                {
+                    if(motJeu[i] == caractere )
+                    {
+                        motAffiche =  motAffiche.Insert(i * 2, caractere.ToString());
+                        motAffiche =  motAffiche.Remove(i * 2 +1, 1);
+                    }
+                }
+
+                lblMotAffiche.Text = motAffiche;
+                cmbLettres.Items.RemoveAt(cmbLettres.SelectedIndex);
+                cmbLettres.SelectedIndex = -1;
+
+            }
+            else
+            {
+                viesJoueur--;
+                imgPendu.Image = pendues[viesJoueur];
+                cmbLettres.SelectedIndex = -1;
+                if (viesJoueur == 0)
+                {
+                    Perdu();
+                }
+            }
+        }
+
+        void Perdu()
+        {
+            lblGagneOuPerdu.Text = "Perdu...";
+            lblMotAffiche.Text = motJeu;
+            InitialisationPartie();
+        }
+
+        void Gagne()
+        {
+            lblGagneOuPerdu.Text = "VOUS AVEZ GAGNE !";
+            InitialisationPartie();
+        }
+
+        //Renvoi True uniquement si le mot est composé uniquement de lettres (majuscule ou minuscule)
+        static bool MotValide(string mot)
+        {
+            bool motValide = true;
+            for(int i=0; i<mot.Length && motValide; i++)
+            {
+                if( !CaractereValide(mot[i]))
+                {
+                    motValide = false;
+                }
+            }
+            return motValide;
+        }
+
+        //Renvoi True uniquement si le caractère est une lettre (majuscule ou minuscule)
+        static bool CaractereValide(char c)
+        {
+            //Si il s'agi d'une Majuscule
+            if( c >='A' && c <='Z')
+            {
+                return true;
+            }
+            //Si c'est une minuscule
+            else if(c >= 'a' && c <= 'z')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void txbUtilisateur_TextChanged(object sender, EventArgs e)
+        {
+            cmbLettres.SelectedIndex = -1;
+        }
+
+        private void cmbLettres_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txbUtilisateur.Text = "";
+        }
+
+        private void btnQuitter_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
